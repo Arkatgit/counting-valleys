e5120914b13302cf0ae0f1d3cc9e96a546f40242
Robert Antwi <robert@mpharma.com>
2020-10-22 17:30:33 +0000

    Properly formated code

--- counting_valleys.go
+++ counting_valleys.go
@@ -3,28 +3,27 @@ package main
 import "fmt"
 
 func countingValleys(steps int, path string) int {
-  numberOfValleys, delta := 0, 0
-  for _, step := range path {
-    if step == 'U' {
-        delta++
-        if delta == 0 {
-          numberOfValleys++
-        }
+	numberOfValleys, delta := 0, 0
+	for _, step := range path {
+		if step == 'U' {
+			delta++
+			if delta == 0 {
+				numberOfValleys++
+			}
 
-      } else {
-        delta--
-      }
+		} else {
+			delta--
+		}
 
-    }
-    return numberOfValleys
+	}
+	return numberOfValleys
 
 }
 
-
-func main(){
-  firstPath, secondPath := "DDUUUUDD", "DDUUUUUDUDDDDUDU"
-  for _, path := range []string{ firstPath, secondPath} {
-      fmt.Println(countingValleys(len(path), path))
-    }
+func main() {
+	firstPath, secondPath := "DDUUUUDD", "DDUUUUUDUDDDDUDU"
+	for _, path := range []string{firstPath, secondPath} {
+		fmt.Println(countingValleys(len(path), path))
+	}
 
 }
