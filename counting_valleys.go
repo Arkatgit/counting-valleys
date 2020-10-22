9fc4a35a9976e7dc6fcc7aed724e8296449d45f3
Robert Antwi <robert@mpharma.com>
2020-10-22 17:01:31 +0000

    Added counting Valleys function and two test cases

---
+++ counting_valleys.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func countingValleys(steps int, path string) int {
+  numberOfValleys, delta := 0, 0
+  for _, step := range path {
+    if step == 'U' {
+        delta++
+        if delta == 0 {
+          numberOfValleys++
+        }
+
+      } else {
+        delta--
+      }
+
+    }
+    return numberOfValleys
+
+}
+
+
+func main(){
+  firstPath, secondPath := "DDUUUUDD", "DDUUUUUDUDDDDUDU"
+  for _, path := range []string{ firstPath, secondPath} {
+      fmt.Println(countingValleys(len(path), path))
+    }
+
+}
